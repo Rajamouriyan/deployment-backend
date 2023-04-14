f58ee8fdedebf0acfd0a16e5233eea423755da61
Rajamouriyan <rsrmouriyan13@gmail.com>
2023-04-14 23:49:38 +0530

    updated backend response

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/', (req, res) => res.send('Backend Application for Deployment Practice'));
 
 // use Routes
 app.use('/api/orders', orders);
